5bc3200d513fc94b008848fbff43402485317d1b
Rui Jiang <me@ruijiang.me>
2026-02-22 00:07:45 -0600

    fixed public path

--- src/views/home.rs
+++ src/views/home.rs
@@ -6,11 +6,14 @@ pub fn Home() -> Element {
         h1 {
             "Hi, I'm Rui."
         }
+        h5 {
+            "Build. Secure. Automate. Scale."
+        }
         p {
             "I'm a software engineer with a strong bias toward systems that are coherent, minimal, and internally correct. I care about structure: in code, in architecture, and in security boundaries. If something works but feels inconsistent, I'll refactor it until it makes sense."
         }
         p {
-            "I'm deeply invested in Linux and open source. I run Arch and NixOS, build my workflow around Niri, WezTerm, Fish, and Neovim, and prefer tools that are transparent and composable. I'm particularly interested in Rust for its emphasis on correctness and explicitness (including building this site as a full-stack Dioxus project)."
+            "I'm deeply invested in Linux and open source. I daily drive Arch and NixOS; build my workflow around Niri, WezTerm, Fish, and Neovim. I'm particularly interested in Rust for its emphasis on correctness and explicitness (including building this site as a full-stack Dioxus project)."
         }
         p {
             "My interests center on systems design and cybersecurity: understanding trust models, attack surfaces, and how small implementation details create large consequences. I'm driven by curiosity, precision, and a desire to understand how things work beneath the abstraction layer."
