47aa874c2c45c47db63b55c03b3ff463a0adaacb
Rui Jiang <me@ruijiang.me>
2026-07-03 13:40:07 -0500

    adjusted spacing

--- src/utils/highlighter.rs
+++ src/utils/highlighter.rs
@@ -107,7 +107,7 @@ impl SyntaxHighlighterAdapter for CustomHighlighter {
             let branch_icon = r#"<svg viewBox="0 0 448 512" width="11" height="12" fill="currentColor" style="display: inline-block; vertical-align: -2px; margin-right: 6px;"><path d="M80 104a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm80-24c0 32.8-19.7 61-48 73.3l0 87.8c18.8-10.9 40.7-17.1 64-17.1l96 0c35.3 0 64-28.7 64-64l0-6.7C307.7 141 288 112.8 288 80c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3l0 6.7c0 70.7-57.3 128-128 128l-96 0c-35.3 0-64 28.7-64 64l0 6.7c28.3 12.3 48 40.5 48 73.3c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-32.8 19.7-61 48-73.3l0-6.7 0-198.7C19.7 141 0 112.8 0 80C0 35.8 35.8 0 80 0s80 35.8 80 80zm232 0a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zM80 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>"#;
             write!(
                 output,
-                "<div class=\"tui-footer\"><span class=\"tui-sec-a\">NORMAL</span><span class=\"tui-sec-b\">{} {}</span><span class=\"tui-sec-c\">{}</span><span class=\"tui-sec-x\">utf-8</span><span class=\"tui-sec-y\">{}</span><span class=\"tui-sec-z\">Top</span></div>",
+                "<div class=\"tui-footer\"><span class=\"tui-sec-a\">NORMAL</span><span class=\"tui-sec-b\">{}{}</span><span class=\"tui-sec-c\">{}</span><span class=\"tui-sec-y\">utf-8</span><span class=\"tui-sec-z\">{}</span></div>",
                 branch_icon,
                 branch,
                 fname,
